b6aa99d6db0dabcba0b421ad26a13e9160db02d8
Senadeera NK <naduni.20191206@iit.ac.lk>
2022-01-29 19:36:32 +0530

    2022/01/29
    created randomColor() function in 'challenge_4' and created the most of html of 'blackjack'.

--- challenge4_change button colors/JS/change button colors.js
+++ challenge4_change button colors/JS/change button colors.js
@@ -5,19 +5,20 @@ var copyAllButtons = [];
  }
  console.log(copyAllButtons);
 
+
 //main function
 function buttonColorChange(buttonThingy){
- console.log(buttonThingy.value); //.value -> to get the value of the option
- if(buttonThingy.value === 'red'){
-   buttonsRed();
- }else if(buttonThingy.value === 'green'){
-    buttonsGreen();
- }else if(buttonThingy.value === 'reset'){
-    buttonColorReset();
-}
-else if(buttonThingy.value === 'random'){
-    randomColors();
-}
+  console.log(buttonThingy.value); //.value -> to get the value of the option
+  if(buttonThingy.value === 'red'){
+    buttonsRed();
+  }else if(buttonThingy.value === 'green'){
+      buttonsGreen();
+  }else if(buttonThingy.value === 'reset'){
+      buttonColorReset();
+  }
+  else if(buttonThingy.value === 'random'){
+      randomColors();
+  }
 }
 
  function buttonsRed(){
@@ -39,5 +40,18 @@ else if(buttonThingy.value === 'random'){
       all_buttons[i].classList.remove(all_buttons[i].classList[1]);
 
       all_buttons[i].classList.add(copyAllButtons[i]);
+   }
+ }
+
+ function randomColors(){
+   var choices = ['btn-primary','btn-danger','btn-warning','btn-success']
+
+   for(let i = 0; i < all_buttons.length;i++){
+     //to give buttons different colors it put in the for loop(different color for different buttons)
+    var randomNo = Math.floor(Math.random()*4); //to 4(without 4)
+    
+    all_buttons[i].classList.remove(all_buttons[i].classList[1]); //removing the second class
+
+    all_buttons[i].classList.add(choices[randomNo]);
    }
  }
