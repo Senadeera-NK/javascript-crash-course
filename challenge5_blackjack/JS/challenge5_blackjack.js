e575302ed4bb1ef167df4fc76381fefb48b8ad14
Senadeera NK <naduni.20191206@iit.ac.lk>
2022-02-03 21:38:22 +0530

    2022/02/03
    updated some in 'challenge5_blackjack.js'. added blackjackDeal function, made showCard function, made 'cards' list, made the randomCard function.

--- challenge5_blackjack/JS/challenge5_blackjack.js
+++ challenge5_blackjack/JS/challenge5_blackjack.js
@@ -1,8 +1,10 @@
-document.querySelector('#blackjack-hit-button').addEventListener('click',BlackJackHit)//CSS selector(dont have to use onclick i nhtml)
+document.querySelector('#blackjack-hit-button').addEventListener('click',BlackJackHit);//CSS selector(dont have to use onclick i nhtml)
 
+document.querySelector('#blackjack-deal-button').addEventListener('click',blackjackDeal);
 let BlackJackGame = {
   'you':{'scoreSpan':'#your-blackjack-result','div':'#your-box','score':0}, //# is important
-  'dealer':{'scoreSpan':'#dealer-blackjack-result','div':'#dealer-box','score':0} //#is important
+  'dealer':{'scoreSpan':'#dealer-blackjack-result','div':'#dealer-box','score':0}, //#is important
+  'cards':['1','2','3','4','5','6','7','8','9','10','J','Q','K','A']
 };
 
 const YOU = BlackJackGame['you'];
@@ -11,9 +13,31 @@ const DEALER = BlackJackGame['dealer'];
 const hitSound = new Audio('SOUNDS/swish.m4a')
 
 function BlackJackHit(){
-  console.log(YOU['div'])
+  let card = randomCard();
+  console.log(card);
+   showCard(DEALER);
+}
+
+function showCard(activePlayer){
+  console.log(activePlayer['div'])
   cardImage = document.createElement('img');
   cardImage.src = 'IMAGES/Q.png';
-  document.querySelector(YOU['div']).appendChild(cardImage);
+  document.querySelector(activePlayer['div']).appendChild(cardImage);
   hitSound.play();
+}
+
+function blackjackDeal(){
+  let yourImages = document.querySelector('#your-box').querySelectorAll('img'); //to get all images in id:your-box
+  let dealerImages = document.querySelector('#dealer-box').querySelectorAll('img'); //to get all images in id:your-box
+  for(let i =0;i < yourImages.length;i++){
+      yourImages[i].remove();
+  }
+  for(let i =0;i < dealerImages.length;i++){
+    dealerImages[i].remove();
+  }
+}
+
+function randomCard(){
+  let randomIndex = Math.floor(Math.random()*13);
+  return BlackJackGame['cards'][randomIndex];
 }
