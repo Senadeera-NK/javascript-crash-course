e5c48cea50c56d4641a9aaea7fec629b9a47779d
Senadeera NK <naduni.20191206@iit.ac.lk>
2022-02-06 21:38:23 +0530

    2022/02/06
    updated in js how to update the wins,losses,draws in table.

--- challenge5_blackjack/JS/challenge5_blackjack.js
+++ challenge5_blackjack/JS/challenge5_blackjack.js
@@ -9,13 +9,18 @@ let BlackJackGame = {
   'you':{'scoreSpan':'#your-blackjack-result','div':'#your-box','score':0}, //# is important
   'dealer':{'scoreSpan':'#dealer-blackjack-result','div':'#dealer-box','score':0}, //#is important
   'cards':['1','2','3','4','5','6','7','8','9','10','J','Q','K','A'],
-  'cardsMap':{'2':2,'3':3,'4':4,'5':5,'6':6,'7':7,'8':8,'9':9,'10':10,'J':10,'Q':10,'K':10,'A':[1,11]}
+  'cardsMap':{'2':2,'3':3,'4':4,'5':5,'6':6,'7':7,'8':8,'9':9,'10':10,'J':10,'Q':10,'K':10,'A':[1,11]},
+  'wins':0,
+  'losses':0,
+  'draws':0
 };
 
 const YOU = BlackJackGame['you'];
 const DEALER = BlackJackGame['dealer'];
 
 const hitSound = new Audio('SOUNDS/swish.m4a')
+const winSound = new Audio('SOUNDS/cash.mp3')
+const lossSound = new Audio('SOUNDS/aww.mp3')
 
 function BlackJackHit(){
   let card = randomCard();
@@ -41,7 +46,7 @@ function showCard(card,activePlayer){
 }
 
 function blackjackDeal(){
-  computeWinner();
+  // showResult(computeWinner());  #maybe for two players put there
   let yourImages = document.querySelector('#your-box').querySelectorAll('img'); //to get all images in id:your-box
   let dealerImages = document.querySelector('#dealer-box').querySelectorAll('img'); //to get all images in id:your-box
   for(let i =0;i < yourImages.length;i++){
@@ -57,6 +62,10 @@ function blackjackDeal(){
 
   document.querySelector('#dealer-blackjack-result').textContent = 0;
   document.querySelector('#dealer-blackjack-result').style.color = 'white';
+
+  document.querySelector('#blackjack-result').textContent = "Let's play";
+  document.querySelector('#blackjack-result').style.color = "black";
+
 }
 
 function updateScore(card,activePlayer){
@@ -87,33 +96,73 @@ function dealerLogic(){
   showCard(card, DEALER);
   updateScore(card, DEALER);
   showScore(DEALER);
+  if(DEALER['score'] > 15){
+   let winner = computeWinner();
+   showResult(winner);
+  }
 }
 
 //compute winner and return who just won the game
+//update the wins/losses/draws
 function computeWinner(){
   let winner;
   if(YOU['score'] <= 21){
     //condition : higher score than dealer or when dealer busts bot you're 21
     if((YOU['score'] > DEALER['score']) || (DEALER['score'] > 21)){
-        console.log('you won!');
+        BlackJackGame['wins'] += 1;
         winner = YOU;
     }else if(YOU['score'] < DEALER['score']){
-        console.log('you lost!');
+        BlackJackGame['losses'] += 1;
         winner = DEALER;
     }else if(YOU['score'] === DEALER['score']){
-        console.log('you drew!');
+        BlackJackGame['draws'] += 1;
     }
 
     //condition: when user busts but dealer doesn't
     }else if(YOU['score'] > 21 && DEALER['score'] <= 21){
-      console.log('you lost!');
+      BlackJackGame['losses'] += 1;
       winner = DEALER;
 
     //when you both busts
     }else if(YOU['score'] > 21 && DEALER['score'] > 21){
-      console.log('you drew!');
+      BlackJackGame['draws'] += 1;
     }
   
-  console.log('winner is: ',winner);
+  console.log(BlackJackGame);
   return winner;
 }
+
+function showResult(winner){
+  let message, messageColor;
+  if(winner === YOU){
+    document.querySelector('#wins').textContent = BlackJackGame['wins'];
+    message='You won !';
+    messageColor='green';
+    winSound.play();
+  }else if(winner === DEALER){
+    document.querySelector('#losses').textContent = BlackJackGame['losses'];
+    message='You lost !';
+    messageColor='red';
+    lossSound.play();
+  }else{
+    document.querySelector('#draws').textContent = BlackJackGame['draws'];
+    message='You drew !';
+    messageColor='black';
+  }
+
+  document.querySelector('#blackjack-result').textContent = message;
+  document.querySelector('#blackjack-result').style.color = messageColor;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
